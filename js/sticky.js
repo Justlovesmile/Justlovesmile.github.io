78f41132ba65aab7114bf178fa65b7bf5ad47db9
Justlovesmile <865717150@qq.com>
2022-06-28 10:10:55 +0800

    Site updated: 2022-06-28 10:10:41

--- js/sticky.js
+++ js/sticky.js
@@ -1,1 +1,15 @@
-var mySwiper=new Swiper("#stickyList",{direction:"horizontal",speed:600,loop:!0,effect:"fade",loopPreventsSlide:!1,autoplay:{delay:2e4},mousewheel:!1,pagination:{el:".swiper-pagination",dynamicBullets:!0}});
+var mySwiper = new Swiper('#stickyList', {
+      direction: 'horizontal',
+      speed: 600,
+      loop: true,
+      effect : 'fade',
+      loopPreventsSlide: false,
+      autoplay: {
+        delay: 20000,
+      },
+      mousewheel: false,
+      pagination: {
+          el: ".swiper-pagination",
+          dynamicBullets: true,
+      },
+    })
